b2982db4fe57389c6238c0d626ac30e5c8259ee7
Lesnov-Petr <lesnov.p@gmail.com>
2021-02-08 13:37:58 +0300

    finish

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,14 +1,14 @@
 import murkup from './markup.js';
 // import myError from './pnotify.js';
-import { error, defaultModules, defaults } from '@pnotify/core/dist/PNotify.js';
+import { error, defaultModules } from '@pnotify/core/dist/PNotify.js';
 import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
-import '@pnotify/core/dist/Material.css';
-import 'material-design-icons/iconfont/material-icons.css';
 
-defaults.styling = 'material';
-defaults.icons = 'material';
-const alarm = 'такой страны не существуе';
+const myError = {
+  text: 'Too many matches found.Please entry a more specific query!',
+};
+const alarm = { text: 'Enter correct data' };
 
 const fetchCountries = searchQuery => {
   const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
@@ -16,14 +16,10 @@ const fetchCountries = searchQuery => {
   return fetch(url)
     .then(res => res.json())
     .then(arrayCountry =>
-      arrayCountry.length < 10
-        ? arrayCountry
-        : error({
-            text: 'Too many matches found.Please entry a more specific query!',
-          }),
+      arrayCountry.length < 10 ? arrayCountry : error(myError),
     )
     .then(country => murkup(country))
-    .catch(error => error({ text: alarm }));
+    .catch(getError => error(alarm));
 };
 
 export default fetchCountries;
